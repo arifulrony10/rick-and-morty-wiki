576d5f6ea6138e78c449c9d6481d2a72b1f6404a
Ariful Islam <ariful.rony10@gmail.com>
2023-05-24 09:48:31 +0600

    TODO: Filter Section

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Search from '../components/Search/Search';
+import UNDERDEVELOPMENT from '../components/UNDERDEVELOPMENT';
 
 const Main = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <UNDERDEVELOPMENT />
         </div>
     );
 };
