135ecdcb13749402fe3bb0fe3f5218f30c5cb515
Ariful Islam <ariful.rony10@gmail.com>
2023-05-29 16:20:08 +0600

    clear filter working

--- src/components/Filters/filters.component.jsx
+++ src/components/Filters/filters.component.jsx
@@ -3,14 +3,25 @@ import SpeciesFilter from "../category/species.component";
 import StatusFilter from "../category/status.component";
 
 const Filters = ({ setPageNumber, setStatus, setGender, setSpecies }) => {
+  const clear = () => {
+    setPageNumber(1);
+    setStatus("");
+    setGender("");
+    setSpecies("");
+    window.location.reload(false);
+  };
+
   return (
     <div className="">
       <h2 className="text center fw-bold fs-4 mb-2">Filters</h2>
-      <div
-        style={{ cursor: "pointer" }}
-        className="text-center text-decoration-underline text-primary "
-      >
-        Clear Filters
+      <div className="d-flex justify-content-center align-items-center">
+        <div
+          style={{ cursor: "pointer" }}
+          className="text-center text-decoration-none btn btn-outline-danger text-dark text-primary mb-4"
+          onClick={clear}
+        >
+          Clear Filters
+        </div>
       </div>
 
       {/* Accordion */}
